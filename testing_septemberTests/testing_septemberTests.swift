a76168b83453da7bb3cbbc97c13f39086d30ddfa
Rayan Mazri <rayan.mazri@epitech.eu>
2025-09-15 19:07:47 +0200

    t

---
+++ testing_septemberTests/testing_septemberTests.swift
@@ -0,0 +1,17 @@
+//
+//  testing_septemberTests.swift
+//  testing_septemberTests
+//
+//  Created by Rayan Mazri on 15/09/2025.
+//
+
+import Testing
+@testable import testing_september
+
+struct testing_septemberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ testing_septemberUITests/testing_septemberUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  testing_septemberUITestsLaunchTests.swift
+//  testing_septemberUITests
+//
+//  Created by Rayan Mazri on 15/09/2025.
+//
+
+import XCTest
+
+final class testing_septemberUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
